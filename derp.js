ae759558715bdc4d5afcfd0e3ea28c0603d27850
Kenparo <34658034+Kenparo@users.noreply.github.com>
2018-04-15 18:17:34 +0300

    reserved for derp java code

--- derp.js
+++ derp.js
@@ -1,9 +1,5 @@
-var image = new Array ();
-image[0] = "derp/dedvris.png";
-image[1] = "http://placehold.it/30";
-image[2] = "http://placehold.it/40";
-image[3] = "http://placehold.it/50";
-var size = image.length
-var x = Math.floor(size*Math.random())
+/*
+magic everywhere in this bitch
 
-$('#random').attr('src',image[x]);
+RESERVER
+*/
